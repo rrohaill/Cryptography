d193a221e5f2e41726dbae268d147bb1546c8251
rohail <rohail@eliq.io>
2019-09-19 10:15:51 +0200

    improvements

--- src/AESEncryptor.kt
+++ src/AESEncryptor.kt
@@ -33,8 +33,7 @@ class AESEncryptor {
                 )
                 ctLength += cipher.doFinal(cipherText, ctLength)
                 return String(
-                    Base64
-                        .encode(cipherText)
+                    Base64.encode(cipherText)
                 )
             }
         } catch (uee: UnsupportedEncodingException) {
--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,13 @@
 fun main(args: Array<String>) {
 
-    val SECRET_KEY: String =
+    val secretKey: String =
         "662ede816988e58fb6d057d9d85605e0"
 
     var encryptor: AESEncryptor = AESEncryptor()
 
-    val encryptedValue: String? =encryptor.encrypt("Rohail", SECRET_KEY)
+    val encryptedValue: String? =encryptor.encrypt("Rohail", secretKey)
     println(encryptedValue)
 
-    val decryptedValue: String? =encryptor.decryptWithAES(SECRET_KEY, encryptedValue)
+    val decryptedValue: String? =encryptor.decryptWithAES(secretKey, encryptedValue)
     println(decryptedValue)
 }
